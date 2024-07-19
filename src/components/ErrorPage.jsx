13c0e3bc30fe4b1e9c164c06b8549cb054eb5948
LuisEduardoRodriguez <97996472+LuisEduardoRodriguez@users.noreply.github.com>
2024-07-19 07:22:12 -0500

    commit erro page

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import './../styles/error-page.css'
+
+export const ErrorPage = () => {
+  return (
+    <div id="custom-body-class">
+        <section id="not-found">
+            <div id="title">404 Error Page</div>
+            <div className='circles'>
+                <p>404
+                    <small>¡Pagina no encontrada!</small>
+                </p>
+                <span className='circle big'></span>
+                <span className='circle med'></span>
+                <span className='circle small'></span>
+            </div>
+        </section>
+    </div>
+  )
+}
